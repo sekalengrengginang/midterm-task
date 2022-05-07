0c9fd84f791e0bce94e29676436d2e7c2844e347
Ahmad <ahmadhawari@students.amikom.ac.id>
2022-05-07 20:31:42 +0700

    membuat konstruktor/properties

--- pemrograman/Program.cs
+++ pemrograman/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+
+    
+
+
+
+
+
+
+
+
+
+
+}
---
+++ pemrograman/dosen.cs
@@ -0,0 +1,48 @@
+class Dosen {
+    //properties
+    public int id  {get; set;}
+    public string nama {get; set;}
+    public int Nik  {get; set;}
+    public string gender  {get; set;}
+    public string course {get; set;}
+
+
+//constructor
+    public  Dosen(){
+        nama = "Kamarudin, M.Kom";
+        course ="Pemrograman";
+    }
+
+    public static void attDosen(){
+
+
+
+
+
+
+
+
+
+    }
+
+    public static void addCourse(){
+
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
