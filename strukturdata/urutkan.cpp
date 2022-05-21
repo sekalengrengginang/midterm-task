bb1ee005232df038daae94bb6867e23fe676dcb5
ueharayumu <ueharayumu@email.com>
2022-05-21 16:10:27 +0700

    fixing something

--- strukturdata/urutkan.cpp
+++ strukturdata/urutkan.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
+using namespace std;
+void insertionNIM(){
+int i,j;
+string temp;
+for (i=0;i<mahasiswa; i++){
 
+temp = NIM[i];
+j = i-1;
+
+while(NIM[j] < temp && j >= 0){
+    NIM[j+1] = NIM[j];
+    j--;
+}
+NIM[j+1] = temp;
+}
+}
